66391aef3c7e9263d2046b4f53c2a9828bf0d9b4
Skyler Liu <vcshing2005@hotmail.com>
2016-10-10 22:18:30 +0800

    wer

--- www/js/index.js
+++ www/js/index.js
@@ -124,7 +124,7 @@ var app = {
 		*/
 
 		app.DeviceonReady();
-		cordova.plugins.backgroundMode.disable();
+		//cordova.plugins.backgroundMode.disable();
         console.log('Received Event: ' + id);
 		//alert(6);
     },
